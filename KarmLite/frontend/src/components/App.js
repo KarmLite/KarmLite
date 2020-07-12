38f8fad949f7d15d30da38b35e059239a83aa37e
puggu <f2016781@pilani.bits-pilani.ac.in>
2020-07-12 15:40:01 +0530

    login_signup_js_css

--- KarmLite/frontend/src/components/App.js
+++ KarmLite/frontend/src/components/App.js
@@ -1,6 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
+import 'KarmLite/frontend/templates/frontend/App.css'
+import {login} from "./components/login"
+import {signup} from "./components/signup"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+
 import Header from './layout/Header'
 import Footer from './layout/Footer'
 
@@ -9,6 +20,14 @@ export class App extends Component {
         return (
             <Fragment>
                 <Header />
+                <Router>
+                    <div className="App-Header">
+                    <Switch>
+                        <Route path="/login" component={login} exact />
+                        <Route path="/signup" component={signup} exact />
+                    </Switch>                        
+                </div>
+                </Router>
 
                 <Footer />
             </Fragment>
--- KarmLite/frontend/src/components/signup.js
+++ KarmLite/frontend/src/components/signup.js
@@ -2,25 +2,20 @@ import React,{Component} from 'react';
 //import logo from './logo.svg';
 import 'KarmLite/frontend/templates/frontend/signup.css';
 
-
-
 import {Button,Form,FormGroup,Label,Input} from 'reactstrap';
-import {GoogleLoginButton} from 'react-social-login-buttons';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import {GoogleLoginButton} from 'react-social-login-buttons'
 
-//function App() 
-export class login extends Component{
+export class signup extends Component{
   render(){
   return (
     <div className="signup-header">
         <Form className="signup-form">
          <h1 className="text-center"><span>KarmLite</span> </h1> 
          <h3 className="text-center">Welcome</h3>
+         <FormGroup>
+           <Label>Name</Label>
+           <Input type = "name" placeholder= "Name"></Input>
+         </FormGroup>
          <FormGroup>
            <Label>Email</Label>
            <Input type = "email" placeholder= "Email"></Input>
@@ -29,23 +24,22 @@ export class login extends Component{
            <Label>Password</Label>
            <Input type = "password" placeholder= "Password"></Input>
          </FormGroup>
-         <Button className= "btn-lg btn-dark btn-block">Log in</Button>
+         <Button className= "btn-lg btn-dark btn-block">Create Account</Button>
         <div className="text-center pt-7">Or</div>
         <div className="gbutton">
         <GoogleLoginButton  align="center" className= "mt3 mb3"/>
         </div>
         </Form>
-        <a className="signup-link"
-          href="/signup"
+        <a className="signin-link"
+          href="/"
           target="_blank"
           //rel="noopener noreferrer"
           >
-            New user? Sign up
+        Already have an account? Click here to Sign in 
         </a>
     </div>
   );
  }
-  
 }
 
 
